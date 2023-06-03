e2ca45cbbe979cf102295bd45e499162c7ba1ad1
Tristan F <LeoDog896@hotmail.com>
2023-06-03 18:50:10 -0400

    chore: init

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+enum Card {
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack, // 11
+    Queen, // 12
+    King, // 13
+}
+
+fn main() {
+    println!("Hello, world!");
+}
