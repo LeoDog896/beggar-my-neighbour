74075a8c97ee8baf328cdc6b5c81eb47383641d9
Tristan F <LeoDog896@hotmail.com>
2023-06-06 18:12:05 -0400

    feat: benchmarking

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 //! implementation of beggar my neighbour card game
+#![feature(test)]
+extern crate test;
 
 mod clearvec;
 mod slicefifo;
@@ -278,5 +280,32 @@ impl Debug for Game {
         }
 
         write!(f, "{s}")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Game;
+    use test::Bencher;
+    
+    #[bench]
+    fn bench_run_game(b: &mut Bencher) {
+        b.iter(|| {
+            let record =
+                &mut Game::from_string("---AJ--Q---------QAKQJJ-QK/-----A----KJ-K--------A---");
+
+            record.play();
+        });
+    }
+
+    #[test]
+    fn world_record_game() {
+        let record =
+            &mut Game::from_string("---AJ--Q---------QAKQJJ-QK/-----A----KJ-K--------A---");
+
+        let stats = record.play();
+
+        assert_eq!(stats.turns, 8_344);
+        assert_eq!(stats.tricks, 1_164);
     }
 }
