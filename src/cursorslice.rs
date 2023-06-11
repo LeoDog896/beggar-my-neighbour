2baf859d5a9b79bf358ef6a650be9054bb810357
Tristan F <LeoDog896@hotmail.com>
2023-06-11 13:15:26 -0400

    Revert "feat: make cursor a ptr"
    This reverts commit 9a063b553cf843e104577d2cd843f26b4cbc76a8.

--- src/cursorslice.rs
+++ src/cursorslice.rs
@@ -3,51 +3,37 @@
 #[derive(Debug, Clone, Copy)]
 pub struct CursorSlice<T: Copy, const N: usize> {
     data: [T; N],
-    cursor: *mut T,
+    cursor: usize,
 }
 
 impl<T: Copy, const N: usize> CursorSlice<T, N> {
-    /// Contract: you must call `init` in the function that owns this struct.
-    pub unsafe fn new() -> Self {
+    pub fn new() -> Self {
         Self {
             data: [unsafe { std::mem::zeroed() }; N],
-            cursor: unsafe { std::mem::zeroed() },
+            cursor: 0,
         }
-    }
-
-    
-    pub fn init(&mut self) {
-        self.cursor = self.data.as_mut_ptr();
     }
 
     /// This is fully unsafe! We are assuming that the cursor is always in bounds in release mode.
     pub unsafe fn push_unchecked(&mut self, value: T) {
-        debug_assert!(self.len() < N, "CursorSlice::push_unchecked: slice is full!");
-        *self.cursor = value;
-        self.cursor = self.cursor.offset(1);
+        debug_assert!(self.cursor < N, "CursorSlice is full!");
+        *self.data.get_unchecked_mut(self.cursor) = value;
+        self.cursor += 1;
     }
 
-    pub fn as_head_ptr(&self) -> *const T {
-        self.data.as_ptr()
-    }
-
-    pub fn len(&self) -> usize {
-        (unsafe { self.cursor.offset_from(self.data.as_ptr() as *mut T) }) as usize
-    }
-
-    fn slice(&self) -> &[T] {
-        unsafe { std::slice::from_raw_parts(self.as_head_ptr(), self.len()) }
+    pub fn slice(&self) -> &[T] {
+        unsafe { self.data.get_unchecked(..self.cursor) }
     }
 
     pub fn clear(&mut self) {
-        self.cursor = self.data.as_mut_ptr();
+        self.cursor = 0;
     }
 
     pub fn iter(&self) -> impl Iterator<Item = &T> {
         self.slice().iter()
     }
 
-    pub fn is_empty(&self) -> bool {
-        self.cursor == self.data.as_ptr() as *mut T
+    pub const fn is_empty(&self) -> bool {
+        self.cursor == 0
     }
 }
