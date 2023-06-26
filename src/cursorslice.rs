1e854cdee21335e8810c384535827ef0f40753e1
Tristan F <LeoDog896@hotmail.com>
2023-06-25 20:33:55 -0400

    feat: faster initialization of cursorslice

--- src/cursorslice.rs
+++ src/cursorslice.rs
@@ -1,15 +1,17 @@
+use std::mem::MaybeUninit;
+
 /// An optimized structure trading off memory for speed.
 /// It is a slice that has a cursor that navigates around, which only supports push and clear.
 #[derive(Debug, Clone, Copy)]
 pub struct CursorSlice<T: Copy, const N: usize> {
-    data: [T; N],
+    data: [MaybeUninit<T>; N],
     cursor: usize,
 }
 
 impl<T: Copy, const N: usize> CursorSlice<T, N> {
     pub fn new() -> Self {
         Self {
-            data: [unsafe { std::mem::zeroed() }; N],
+            data: unsafe { MaybeUninit::uninit().assume_init() },
             cursor: 0,
         }
     }
@@ -17,12 +19,14 @@ impl<T: Copy, const N: usize> CursorSlice<T, N> {
     /// This is fully unsafe! We are assuming that the cursor is always in bounds in release mode.
     pub unsafe fn push_unchecked(&mut self, value: T) {
         debug_assert!(self.cursor < N, "CursorSlice is full!");
-        *self.data.get_unchecked_mut(self.cursor) = value;
+        let ptr = self.data.get_unchecked_mut(self.cursor) as *mut MaybeUninit<T> as *mut T;
+        ptr.write(value);
         self.cursor += 1;
     }
 
     pub fn slice(&self) -> &[T] {
-        unsafe { self.data.get_unchecked(..self.cursor) }
+        let slice = &self.data[..self.cursor];
+        unsafe { &*(slice as *const [MaybeUninit<T>] as *const [T]) }
     }
 
     pub fn clear(&mut self) {
